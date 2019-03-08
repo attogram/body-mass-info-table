80484458d066d5ab930b351b10ad2351e4b34215
Attogram Project <attogram@users.noreply.github.com>
2019-03-08 22:40:11 +0100

    autoload check

--- public/index.php
+++ public/index.php
@@ -3,7 +3,11 @@ declare(strict_types = 1);
 
 namespace Attogram\Body;
 
-require_once('../vendor/autoload.php');
+$autoload = '../vendor/autoload.php';
+if (!is_readable($autoload)) {
+    die('Vendor autoload file not found.  Please run composer install.');
+}
+require_once($autoload);
 
 $web = new Web();
 
