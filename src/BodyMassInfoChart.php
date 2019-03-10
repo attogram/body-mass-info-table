bf1f9639e791eb89d348cd97c53394005bad393b
Attogram Project <attogram@users.noreply.github.com>
2019-03-10 22:54:15 +0100

    v1.5.2

--- src/BodyMassInfoChart.php
+++ src/BodyMassInfoChart.php
@@ -11,7 +11,7 @@ use Attogram\Router\Router;
 class BodyMassInfoChart
 {
     /** @var string Version*/
-    const VERSION = '1.5.1';
+    const VERSION = '1.5.2';
 
     /** @var Router */
     private $router;
