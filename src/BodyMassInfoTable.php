2e4a685a95cba3dc2ca58f800fac344f079c8093
Attogram Project <attogram@users.noreply.github.com>
2019-03-18 21:19:16 +0100

    v1.9.3

--- src/BodyMassInfoTable.php
+++ src/BodyMassInfoTable.php
@@ -14,7 +14,7 @@ class BodyMassInfoTable
     use TemplateTrait;
 
     /** @var string Version*/
-    const VERSION = '1.9.2';
+    const VERSION = '1.9.3';
 
     /** @var Router */
     private $router;
